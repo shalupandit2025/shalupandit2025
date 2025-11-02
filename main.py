3085238ba5a739303c5909ffb60d9cf9b0c32ef5
shalu panit <shalupandit2025@gmail.com>
2025-11-03 00:45:54 +0530

    my resume.html

--- main.py
+++
@@ -1,7 +0,0 @@
-print("hello world")echo "# shalupandit2025" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/shalupandit2025/shalupandit2025.git
-git push -u origin main
